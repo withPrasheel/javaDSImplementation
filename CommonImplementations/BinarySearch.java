50161d731032ec7d6f7332958ce3892d5f3c75da
Prasheel <38638028+withPrasheel@users.noreply.github.com>
2023-10-21 20:07:56 -0700

    Create BinarySearch.java

---
+++ CommonImplementations/BinarySearch.java
@@ -0,0 +1,14 @@
+ublic int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(end>=start){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid]>target)
+                end = mid-1;
+            else 
+                start = mid+1;
+        }
+        return -1;
+    }
